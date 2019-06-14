18f62c077df43ee290817d604c4074a1350b1a6c
Jean-Pierre Bolet <jpbolet@gmail.com>
2019-06-14 18:01:34 +0100

    remove comments

--- app.js
+++ app.js
@@ -39,13 +39,9 @@ app.get("/", (req, res) => {
 // Get 'code' querystring parameter and hit data api
 app.get("/linkedin-redirect", async (req, res) => {
 
-console.log("linkedin-redirect getCurrentMemberProfile error:" + req.query.code);
-// res.send("linkedin-redirect:" + req.query.code);
+  console.log("linkedin-redirect getCurrentMemberProfile error:" + req.query.code);
 
   try {
-    // var token = linkedInAPI.getAccessToken(req.query.code, req.query.state);
-    // res.send("linkedin-redirect token:" + token);
-
 
     linkedInAPI.getAccessToken(req.query.code, req.query.state)
     .then(response => {
@@ -60,26 +56,6 @@ console.log("linkedin-redirect getCurrentMemberProfile error:" + req.query.code)
       console.log("linkedin-redirect getAccessToken error:" + err);
     });
 
-
-    //linkedInAPI.getAccessToken(res, req.query.code, req.query.state, function(err, results) {
-
-    // if ( err )
-    //     return console.error(err);
-
-    //console.log(JSON.stringify(results));
-
-    // linkedInAPI.init(results.access_token);
-    // linkedInAPI.getCurrentMemberProfile(['id', 'firstName', 'lastName'], results.access_token)
-    // .then(response => {
-    //   console.log("linkedin-redirect getCurrentMemberProfile:" + response);
-    //   res.send("Member:" + JSON.stringify(response));
-    //   done();
-    // })
-    // .catch(err => {
-    //   console.log("linkedin-redirect getCurrentMemberProfile error:" + err);
-    //   done()
-    // });
-  // });
   } catch (error2) {
     console.log("linkedin-redirect getAccessToken error2:" + error2);
   }
